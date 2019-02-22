837e1aa93d39f1465660b4c90aa53d945bd8f456
Ammaarah <ammaarah.desai@gmail.com>
2019-02-22 22:27:38 +0200

    Added 3 additional test

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -20,6 +20,12 @@ namespace Tests
             Assert.AreEqual("0,0:", result1);
             string result2 = p.Prefix("what ... did you say?? ");
             Assert.AreEqual("23,5:what ... did you say?? ", result2);
+            string result3 = p.Prefix("rainbows sunshine and unicorns");
+            Assert.AreEqual("30,4:rainbows sunshine and unicorns", result3);
+            string result4 = p.Prefix(null);
+            Assert.AreEqual(null, result4);
+            string result5 = p.Prefix("it's a   wonderful    world!!!!");
+            Assert.AreEqual("31,4:it's a   wonderful    world!!!!", result5);
         }
         
     }
